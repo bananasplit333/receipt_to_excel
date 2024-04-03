885ff131f4cf76ff0e8548fa6764485759495f38
bananasplit333 <134814180+bananasplit333@users.noreply.github.com>
2024-04-02 22:13:03 -0400

    a

--- app.py
+++ app.py
@@ -6,6 +6,15 @@ from flask_cors import cross_origin, CORS
 app = Flask(__name__)
 CORS(app)
 
+@app.after_request
+def add_cors_headers(response):
+    origin = request.headers.get('Origin')
+    if origin and origin.startswith('https://drag-and-drop-nextjs-bntiql1xb-bananasplit333s-projects.vercel.app/'):
+        response.headers['Access-Control-Allow-Origin'] = origin
+        response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
+        response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    return response
+
 @app.route('/process-receipts', methods=['POST'])
 def process_receipts():
     # Get the uploaded image files
